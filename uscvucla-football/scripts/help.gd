774cb2725642a1b403f9a9ab6500484c3c88dbaa
Jon Moon <jonmoon@Mac.attlocal.net>
2024-11-14 01:20:02 -0800

    added tutorial

--- uscvucla-football/scripts/help.gd
+++ uscvucla-football/scripts/help.gd
@@ -1,17 +1,28 @@
 extends Node
+var arrow_rot: bool = true
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$Sprite2D2.rotation_degrees = -40
 	if !Global.firstThrow:
 		queue_free()
 	else:
 		$".".visible = true
+	$AnimatedSprite2D.play("spacebar")
+	$AnimatedSprite2D2.play("tutorial_receiver")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if arrow_rot: 
+		$Sprite2D2.rotation_degrees += 1
+		if $Sprite2D2.rotation_degrees >= 40:
+			arrow_rot = false
+	else:
+		$Sprite2D2.rotation_degrees -= 1
+		if $Sprite2D2.rotation_degrees <= -40:
+			arrow_rot = true
 
 
 func _on_player_start_throw() -> void:
