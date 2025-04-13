161ce9108f4e831290f29feade4f2b1ec9bd7a23
Jon Moon <jonmoon@Mac.attlocal.net>
2025-04-13 11:59:38 -0700

    fixed bug that registered a click in main menu as a click in-game

--- uscvucla-football/scripts/title_screen.gd
+++ uscvucla-football/scripts/title_screen.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Control
 
 @onready var runner1 = $SubViewportContainer/SubViewport/AnimatedSprite2D
 @onready var runner2 = $SubViewportContainer/SubViewport/AnimatedSprite2D2
@@ -6,14 +6,13 @@ var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	rng.randomize()
 	spawn_runners()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("title_accept"):
-		var level_scene = preload("res://scenes/test.tscn").instantiate()
-		get_tree().root.add_child(level_scene)
-		queue_free()
+		get_tree().change_scene_to_file("res://scenes/test.tscn")
 		
 	runner1.position.x += 200*delta
 	runner2.position.x -= 200*delta
